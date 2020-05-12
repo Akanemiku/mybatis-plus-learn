bdb87c20cccb03a1c8ea4d6290680b763254f383
Akanemiku <806394913@qq.com>
2020-05-12 17:59:18 +0800

    add delete

--- src/test/java/github/akanemiku/mybatispluslearn/OtherTest.java
+++ src/test/java/github/akanemiku/mybatispluslearn/OtherTest.java
@@ -1,5 +1,6 @@
 package github.akanemiku.mybatispluslearn;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
@@ -13,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -26,7 +30,7 @@ public class OtherTest {
      * 见：https://mp.baomidou.com/guide/faq.html
      */
     @Test
-    public void insert(){
+    public void insert() {
         User user = new User();
         user.setName("王小明");
         user.setAge(17);
@@ -40,7 +44,7 @@ public class OtherTest {
      * 根据id更新
      */
     @Test
-    public void updateById(){
+    public void updateById() {
         User user = new User();
         user.setId(1088248166370832385L);
         user.setAge(28);
@@ -53,13 +57,13 @@ public class OtherTest {
      * 根据实体更新
      */
     @Test
-    public void updateByWrapper(){
+    public void updateByWrapper() {
         UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<User>();
-        userUpdateWrapper.eq("name","李艺伟").eq("age",28);
+        userUpdateWrapper.eq("name", "李艺伟").eq("age", 28);
         User user = new User();
         user.setEmail("lyw2@baomidou.com");
         user.setAge(27);
-        int rows = mapper.update(user,userUpdateWrapper);
+        int rows = mapper.update(user, userUpdateWrapper);
         System.out.println(rows);
     }
 
@@ -67,10 +71,10 @@ public class OtherTest {
      * 字段少时
      */
     @Test
-    public void updateByWrapper2(){
+    public void updateByWrapper2() {
         UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<User>();
-        userUpdateWrapper.eq("name","李艺伟").eq("age",26).set("age",30);
-        int rows = mapper.update(null,userUpdateWrapper);
+        userUpdateWrapper.eq("name", "李艺伟").eq("age", 26).set("age", 30);
+        int rows = mapper.update(null, userUpdateWrapper);
         System.out.println(rows);
     }
 
@@ -78,10 +82,10 @@ public class OtherTest {
      * lambda表达式版
      */
     @Test
-    public void updateByWrapperLambda(){
+    public void updateByWrapperLambda() {
         LambdaUpdateWrapper<User> lambdaUpdateWrapper = Wrappers.<User>lambdaUpdate();
-        lambdaUpdateWrapper.eq(User::getName,"李艺伟").eq(User::getAge,30).set(User::getAge,31);
-        int rows = mapper.update(null,lambdaUpdateWrapper);
+        lambdaUpdateWrapper.eq(User::getName, "李艺伟").eq(User::getAge, 30).set(User::getAge, 31);
+        int rows = mapper.update(null, lambdaUpdateWrapper);
         System.out.println(rows);
     }
 
@@ -89,10 +93,52 @@ public class OtherTest {
      * 新版写法
      */
     @Test
-    public void updateByWrapperLambdaChain(){
+    public void updateByWrapperLambdaChain() {
         boolean update = new LambdaUpdateChainWrapper<User>(mapper)
-                .eq(User::getName,"李艺伟").eq(User::getAge,30).set(User::getAge,31).update();
+                .eq(User::getName, "李艺伟").eq(User::getAge, 30).set(User::getAge, 31).update();
         System.out.println(update);
     }
+
+    /**
+     * 根据id删除
+     */
+    @Test
+    public void deleteById() {
+        int rows = mapper.deleteById(1259855205395120129L);
+        System.out.println(rows);
+    }
+
+    /**
+     * 根据map删除
+     */
+    @Test
+    public void deleteByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "王小明");
+        map.put("age", 17);
+        int rows = mapper.deleteByMap(map);
+        System.out.println(rows);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Test
+    public void deleteBatchIds() {
+        int rows = mapper.deleteBatchIds(Arrays.asList(1259855205395120129L, 1094592041087729666L));
+        System.out.println(rows);
+    }
+
+    /**
+     * lambda表达式版
+     */
+    @Test
+    public void deleteByWrapper() {
+        LambdaQueryWrapper<User> lambdaQueryWrapper = Wrappers.<User>lambdaQuery();
+        lambdaQueryWrapper.eq(User::getAge,16).or().gt(User::getAge,40);
+        int rows = mapper.delete(lambdaQueryWrapper);
+        System.out.println(rows);
+    }
+
 
 }
